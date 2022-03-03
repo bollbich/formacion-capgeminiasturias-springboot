a96d893aaa1dae1ccb65411a2908aed236dd258f
Rainiero Fresno Duval <rainierofresno@gmail.com~>
2022-03-03 11:48:14 +0100

    Faltaba el @sevice del servicio de usuarios

--- apirest.RRHH/src/main/java/com/formacionsprongboot/apirest/RRHH/service/UsuarioServiceImpl.java
+++ apirest.RRHH/src/main/java/com/formacionsprongboot/apirest/RRHH/service/UsuarioServiceImpl.java
@@ -3,10 +3,12 @@ package com.formacionsprongboot.apirest.RRHH.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.formacionsprongboot.apirest.RRHH.dao.UsuarioDao;
 import com.formacionsprongboot.apirest.RRHH.entity.Usuario;
 
+@Service
 public class UsuarioServiceImpl implements UsuarioService {
 
 	@Autowired
