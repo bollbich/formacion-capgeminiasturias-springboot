cd9710e9854b36f9d4357141ef580e8f81909dfc
Rainiero Fresno Duval <rainierofresno@gmail.com~>
2022-03-22 09:13:07 +0100

    Conectamos con el front de angular

--- apirest.clientes/src/main/java/com/formacionsprongboot/apirest/controller/ClienteRestController.java
+++ apirest.clientes/src/main/java/com/formacionsprongboot/apirest/controller/ClienteRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.formacionsprongboot.apirest.entity.Cliente;
 import com.formacionsprongboot.apirest.service.ClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
